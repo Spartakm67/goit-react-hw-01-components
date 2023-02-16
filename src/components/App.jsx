232ae804ef7fc042ebe2df21321b17b10271a701
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-02-16 15:47:23 +0200

    Transactions Added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,12 @@
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
-// import transactions from '../data/transactions.json';
+import transactions from '../data/transactions.json';
 
 import Profile from './Profile/UserProfile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -25,14 +26,9 @@ export const App = () => {
      <Statistics title="Upload stats" stats={data} />
      <Statistics title="" stats={data} />
      <FriendList friends={friends} />
-     
+     <TransactionHistory transactions={transactions} />
     </div>
-    
-    
       
   );
 };
-/* <Statistics title="Upload stats" stats={data} /> */
-/* <Statistics stats={data} />
-     <FriendList friends={friends} />
-     <TransactionHistory items={transactions} /> */
+ 
